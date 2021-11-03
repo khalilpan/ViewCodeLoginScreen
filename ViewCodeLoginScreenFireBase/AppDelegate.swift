8884751aceffee3ca1fb8c54a872ed4df3d406cd
Khalil Panahi <khalil.panahi@accenture.com>
2021-11-03 09:41:35 -0300

    initial commit

--- ViewCodeLoginScreenFireBase/AppDelegate.swift
+++ ViewCodeLoginScreenFireBase/AppDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+
+               // set root view controller the window
+               let controller = LoginController()
+               window.rootViewController = controller
+
+               self.window = window
+               window.makeKeyAndVisible()
+        
         return true
     }
 
