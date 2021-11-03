f0e3b6edd99dcf6c3f91ab7d57e3edcd814199f2
Khalil Panahi <khalil.panahi@accenture.com>
2021-11-03 09:35:23 -0300

    Initial Commit

---
+++ ViewCodeLoginScreenFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewCodeLoginScreenFireBase
+//
+//  Created by khalil.panahi on 03/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
